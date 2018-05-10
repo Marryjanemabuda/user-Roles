9079b35229ccb8898a539894710f994b37a76e6a
Khabubu Phathutshedzo <khabubuphathu@gmail.com>
2018-05-10 22:00:02 +0200

    [phathu] : Added Design Time Database Creation

---
+++ UserRoles/UserRoles/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserRoles.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>();
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-UserRoles-E0161990-E3EA-439A-8D65-E299AA9C163B;Trusted_Connection=True;MultipleActiveResultSets=true");
+            return new ApplicationDbContext(options.Options);
+        }
+    }
+}
